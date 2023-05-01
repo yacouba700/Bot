3047fc6f247f7f476bff83fcb021e2afc11c91e0
yacouba700 <108770173+yacouba700@users.noreply.github.com>
2023-05-01 15:09:07 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const { scrapeLogic } = require("./scrapeLogic");
+const cors = require('cors');
 const app = express();
 
+app.use(cors())
 const PORT = process.env.PORT || 4000;
 
 app.get("/scrape", (req, res) => {
