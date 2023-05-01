3e60160429c92203ddeef1359e8a7300d76e1125
yacouba700 <108770173+yacouba700@users.noreply.github.com>
2023-05-01 12:26:38 +0000

    Update scrapeLogic.js

--- scrapeLogic.js
+++ scrapeLogic.js
@@ -47,7 +47,7 @@ const scrapeLogic = async (res) => {
       printBackground: true,
       format: "A4",
     });
-    res.sendFile(pdfs);
+    res.sendFile("result.pdf");
   } catch (e) {
     console.error(e);
     res.send(`Something went wrong while running Puppeteer: ${e}`);
