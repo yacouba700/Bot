5065e99d2fe8b70259e1bbb7a098d47c11766657
yacouba700 <108770173+yacouba700@users.noreply.github.com>
2023-05-01 14:43:02 +0000

    Update scrapeLogic.js

--- scrapeLogic.js
+++ scrapeLogic.js
@@ -40,8 +40,8 @@ const scrapeLogic = async (res) => {
     // Print the full title
     //const logStatement = `The title of this blog post is ${fullTitle}`;
     //console.log(logStatement);
-   // const content = `<h1> bonjour</h1>`;
-    page.setContent(html("Yacouba","3000","0","intermédiaire", "non payer ")
+   // const content = html("Yacouba","3000","0","intermédiaire", "non payer ");
+    page.setContent(content)
     const pdfs = await page.pdf({
       path: "result.pdf",
       margin: { top: "100px", right: "50px", bottom: "100px", left: "50px" },
